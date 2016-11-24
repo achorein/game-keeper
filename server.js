deee78142c6bb085bb363d58dcb8d5112baebed1
root <root@seraphim.fr>
2016-11-24 23:43:14 +0100

    Gestion des cors et utilisation de la route /api

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var app = express();
 // parse application/json
 app.use(bodyParser.json());
+app.use(cors());
 
 var Datastore = require('nedb');
 var db = new Datastore({ filename: 'game.db', autoload: true });
 
-app.get('/', function(req, res) {
+app.get('/api', function(req, res) {
     res.send('Game Keeper');
 });
 
 // Récupération du highscore
-app.get('/score/max', function(req, res) {
+app.get('/api/score/max', function(req, res) {
     db.findOne({ score: { $exists: true }}).sort({ score: -1, date: -1 }).limit(1).exec(function(err, value) {
         if (err) return console.log('Ooops!', err);
-        console.log('score max =' + JSON.stringify(value));
+        //console.log('score max =' + JSON.stringify(value));
+	if (value == null) {
+	    value = {name: '', score: 0}
+        }
         res.send(value);
     });
 });
 
 // Récupération des 10 meilleurs scores
-app.get('/score/top', function(req, res) {
+app.get('/api/score/top', function(req, res) {
     db.find({ score: { $exists: true }}).sort({ score: -1, date: -1 }).limit(10).exec(function(err, values) {
         if (err) return console.log('Ooops!', err);
-        console.log(JSON.stringify(values));
+        //console.log(JSON.stringify(values));
         res.send(values);
     });
 });
 
 // Ajout d'un nouveau score
-app.put('/score', function (req, res) {
-    var data = req.body.data;
+app.put('/api/score', function (req, res) {
+    var data = req.body;
     db.insert({
         name: data.name,
         player: data.player,
